011b873088536404635dec6eeaafcb866e93a2d5
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2021-06-09 18:08:07 -0300

    first commit

---
+++ bayesian-network.py
@@ -0,0 +1,38 @@
+class BayesianNetwork:
+    def __init__(self, nodos, tablasDeProbabilidades) -> None:
+        """
+        nodos: [str] 
+            lista de nombres de nodos 
+            
+                                   
+        tablasDeProbabilidades: 
+            P(J/x_0,...,x_n)
+            "x_0...x_n"      "J"  "probabilidad"
+            lista de tuplas que representan arista y probabilidad
+            ejemplo: 
+                    {
+                        ((('a', True), ('b', False)), ('c', True)) : 0.4, 
+                        ((('a', False)), ('c', True)) : 0.7,
+                        ((), ('a', False)) : 0,4
+                    }
+        """
+        self.nodos = nodos
+        self.tablasDeProbabilidades = tablasDeProbabilidades
+        self.dependencias = {}
+        for inputVariablesConValores, (outputVariable, _) in tablasDeProbabilidades:
+            inputVariables = [variable for variable, valor in inputVariablesConValores]
+
+            if outputVariable in self.dependencias:
+                assert(self.dependencias[outputVariable] == inputVariables)
+
+            else:
+                self.dependencias[outputVariable] = inputVariables
+
+
+    def probabilidadDeInstanciaCompleta(self, instancia):
+        """
+        instancia: 
+            ejemplo: {'a': True, 'b': False, ...]
+        """
+        for nodo, valor in instancia.values():
+            dependencias = self.dependencias[nodo]
