6382346bf6124f5146638ecc618620ad57e68837
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2021-06-09 19:18:33 -0300

    Instancias parciales

--- bayesian-network.py
+++ bayesian-network.py
@@ -1,5 +1,8 @@
+from itertools import product
+from pprint import pprint
+
 class BayesianNetwork:
-    def __init__(self, tablasDeProbabilidades) -> None:
+    def __init__(self, nodos, tablasDeProbabilidades) -> None:
         """         
         tablasDeProbabilidades: 
             P(J/x_0,...,x_n)
@@ -7,11 +10,12 @@ class BayesianNetwork:
             lista de tuplas que representan arista y probabilidad
             ejemplo: 
                     {
-                        ((('a', True), ('b', False)), ('c', True)) : 0.4, 
-                        ((('a', False)), ('b', True)) : 0.7,
-                        ((), ('a', False)) : 0,4
+                        ((('a', 1), ('b', 0)), ('c', 1)) : 0.4, 
+                        ((('a', 0)), ('b', 1)) : 0.7,
+                        ((), ('a', 0)) : 0,4
                     }
         """
+        self.nodos = nodos
         self.tablasDeProbabilidades = tablasDeProbabilidades
         self.dependencias = {}
         for inputVariablesConValores, (outputVariable, _) in tablasDeProbabilidades:
@@ -27,7 +31,7 @@ class BayesianNetwork:
     def probabilidadDeInstanciaCompleta(self, instancia):
         """
         instancia: 
-            ejemplo: {'a': True, 'b': False, ...]
+            ejemplo: {'a': 1, 'b': 0, ...]
         """
         resultado = 1
         
@@ -40,74 +44,93 @@ class BayesianNetwork:
 
         return resultado
 
+    def probabilidadDeInstancia(self, instancia):
+        nodosNoInstanciados = [nodo for nodo in self.nodos if nodo not in instancia]
+
+        tablaDeVerdad = list(product([False, True], repeat=len(nodosNoInstanciados)))
+
+        instanciasCompletas = []
+        for registroDeVerdad in tablaDeVerdad:
+            instanciaCompleta = instancia.copy()
+            for nodo, valor in zip(nodosNoInstanciados, registroDeVerdad):
+                instanciaCompleta[nodo] = valor
+
+            instanciasCompletas.append(instanciaCompleta)
+
+        resultado = 0
+        for instanciaCompleta in instanciasCompletas:
+            resultado += self.probabilidadDeInstanciaCompleta(instanciaCompleta)
+
+        return resultado
+
 tablaAlarma = {
     # Robo
     (
         (), 
-        ('r', True)) : 0.001,
+        ('r', 1)) : 0.001,
     (
         (), 
-        ('r', False)) : 0.999,
+        ('r', 0)) : 0.999,
     # Temblor
     (
         (), 
-        ('t', True)) : 0.002,
+        ('t', 1)) : 0.002,
     (
         (), 
-        ('t', False)) : 0.998,
+        ('t', 0)) : 0.998,
     # Alarma
     (
-        (('r', True), ('t', True)), 
-        ('a', True))   : 0.950,
+        (('r', 1), ('t', 1)), 
+        ('a', 1))   : 0.950,
     (
-        (('r', True), ('t', False)), 
-        ('a', True))  : 0.950,
+        (('r', 1), ('t', 0)), 
+        ('a', 1))  : 0.94,
     (
-        (('r', False), ('t', True)), 
-        ('a', True))  : 0.290,
+        (('r', 0), ('t', 1)), 
+        ('a', 1))  : 0.290,
     (
-        (('r', False), ('t', False)), 
-        ('a', True)) : 0.001,
+        (('r', 0), ('t', 0)), 
+        ('a', 1)) : 0.001,
     (
-        (('r', True), ('t', True)), 
-        ('a', False))   : 0.050,
+        (('r', 1), ('t', 1)), 
+        ('a', 0))   : 0.050,
     (
-        (('r', True), ('t', False)), 
-        ('a', False))  : 0.050,
+        (('r', 1), ('t', 0)), 
+        ('a', 0))  : 0.051,
     (
-        (('r', False), ('t', True)), 
-        ('a', False))  : 0.710,
+        (('r', 0), ('t', 1)), 
+        ('a', 0))  : 0.710,
     (
-        (('r', False), ('t', False)), 
-        ('a', False)) : 0.999,
+        (('r', 0), ('t', 0)), 
+        ('a', 0)) : 0.999,
     # Juan
     (
-        (('a', True),), 
-        ('j', True)) : 0.90,
+        (('a', 1),), 
+        ('j', 1)) : 0.90,
     (
-        (('a', False),), 
-        ('j', True)) : 0.05,
+        (('a', 0),), 
+        ('j', 1)) : 0.05,
     (
-        (('a', True),),
-         ('j', False)) : 0.10,
+        (('a', 1),),
+         ('j', 0)) : 0.10,
     (
-        (('a', False),), 
-        ('j', False)) : 0.95,
+        (('a', 0),), 
+        ('j', 0)) : 0.95,
     # Maria
     (
-        (('a', True),), 
-        ('m', True)) : 0.70,
+        (('a', 1),), 
+        ('m', 1)) : 0.70,
     (
-        (('a', False),), 
-        ('m', True)) : 0.01,
+        (('a', 0),), 
+        ('m', 1)) : 0.01,
     (
-        (('a', True),),
-         ('m', False)) : 0.30,
+        (('a', 1),),
+         ('m', 0)) : 0.30,
     (
-        (('a', False),), 
-        ('m', False)) : 0.99,
+        (('a', 0),), 
+        ('m', 0)) : 0.99,
 }
 
-red = BayesianNetwork(tablaAlarma)
+red = BayesianNetwork(['r', 't', 'a', 'j', 'm'], tablaAlarma)
 
-print(red.probabilidadDeInstanciaCompleta({'r': True, 't': True, 'a': True, 'j': True, 'm': True}))
+print(red.probabilidadDeInstancia({'j': 1}))
